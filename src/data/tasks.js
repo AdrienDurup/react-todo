23f89715482f44af60abae9ee06a0e6f4352b8e6
Simon MARTIN <simon@oclock.io>
2021-12-10 17:19:22 +0100

    Initial commit

---
+++ src/data/tasks.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id: 1,
+    label: 'Coder une todolist en impératif',
+    done: true,
+  },
+  {
+    id: 13,
+    label: 'Coder une todolist en React',
+    done: false,
+  },
+  {
+    id: 4,
+    label: 'Appeler Jean-Marc',
+    done: false,
+  },
+  {
+    id: 8,
+    label: 'Préparer des crêpes',
+    done: false,
+  },
+];
